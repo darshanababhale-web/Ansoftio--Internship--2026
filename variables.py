0e041ac8463dc2f025b7a37c7fd7527264208105
Darshana Babhale <darshanababhale@gmail.com>
2026-03-10 20:14:05 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Student Information using Variables
+
+student_name = "Darshana Babhale"
+age = 21
+city = "Ahilyanagar"
+course = "Python Developer Internship"
+
+print("Student Name:", student_name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
