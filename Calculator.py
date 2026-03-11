3c2ab15780f3c686527044e5e4cf4649a6414379
Darshana Babhale <darshanababhale@gmail.com>
2026-03-11 12:53:47 +0530

    Create Calculator.py
    Calculator program

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# Simple Calculator Program
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
