bf03a3b03ff8da941629ef3886c69618120344c7
Darshana Babhale <darshanababhale@gmail.com>
2026-03-12 11:10:47 +0530

    Create user_input.py
    Input

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# Taking user input
+
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+city = input("Enter your City: ")
+course = input("Enter your Course: ")
+
+# Printing the entered information
+print("\n--- User Information ---")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
