5d0f646e73879355f589003b77506a356f9bb00c
Darshana Babhale <darshanababhale@gmail.com>
2026-03-17 15:40:28 +0530

    Create tuple_program.py
    https://github.com/yourname/repo/Day-9/tuple_program.py)

---
+++ Day-9/tuple_program.py
@@ -0,0 +1,11 @@
+# Create a tuple of 5 subjects
+subjects = ("Math", "Science", "English", "History", "Computer")
+
+# Print all subjects
+print("All Subjects:", subjects)
+
+# Print first subject
+print("First Subject:", subjects[0])
+
+# Print last subject
+print("Last Subject:", subjects[-1])
