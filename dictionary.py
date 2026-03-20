0c13f0d8fbb0d5da9a843b7d6073c790d208fdf4
Darshana Babhale <darshanababhale@gmail.com>
2026-03-20 19:16:12 +0530

    Create dictionary.py
    11 day

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# Create dictionary
+student = {
+    "Name": "Darshana",
+    "Age": 22,
+    "City": "Ahmednagar",
+    "Course": "Python"
+}
+
+# Print all values
+print("Student Details:")
+print("Name:", student["Name"])
+print("Age:", student["Age"])
+print("City:", student["City"])
+print("Course:", student["Course"])
