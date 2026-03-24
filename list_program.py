6309be2cf6f2d70625eda44e398bff5a3e49d4ee
Darshana Babhale <darshanababhale@gmail.com>
2026-03-24 19:50:47 +0530

    Create list_program.py

---
+++ list_program.py
@@ -0,0 +1,11 @@
+# Creating list
+numbers = [10, 25, 5, 40, 15]
+
+# Print all numbers
+print("All numbers:", numbers)
+
+# Maximum number
+print("Maximum number:", max(numbers))
+
+# Minimum number
+print("Minimum number:", min(numbers))
